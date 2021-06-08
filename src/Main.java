f0d70a34eb3ec079fa00080e751173bff9ee1b99
ejgoodwin <ericagoodwin90@gmail.com>
2021-06-08 22:52:20 +0100

    added main.java to default package

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import game.Board;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Board board = new Board();
+		board.startUI();
+
+	}
+
+}
