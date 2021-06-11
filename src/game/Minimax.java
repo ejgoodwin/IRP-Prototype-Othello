e445b9047472e6a61c69340b3f302bfa181ff4f0
ejgoodwin <ericagoodwin90@gmail.com>
2021-06-11 20:49:37 +0100

    tidy comments

--- src/game/Minimax.java
+++ src/game/Minimax.java
@@ -48,7 +48,6 @@ public class Minimax {
 		counter++;
 		ArrayList<Integer> availSquares = evaluateBoard(boardMinMAx, player);
 		// If end of depth, see who has the best score.
-		//System.out.println(availSquares);
 		if (depth == level || availSquares.size() < 1) {
 			int score = boardValue(player, boardMinMAx, availSquares.size());
 			Map<String, Integer> scoreReturn = new HashMap<>();
@@ -67,10 +66,9 @@ public class Minimax {
 				logic.setBoard(boardMinMAx);
 				logic.checkNextItem();
 				char[] newBoard = logic.getNewBoard();
-				//System.out.println(newBoard);
-				// store result of minimax
+				// Store result of minimax.
 				Map<String, Integer> result = minimaxSearch(newBoard, nextPlayer, depth+1);
-				// Find the MAXIMUM score
+				// Find the MAXIMUM score.
 				if (result.get("score").doubleValue() > bestScore.get("score").doubleValue()) {	
 					bestScore.put("score", result.get("score"));
 					bestScore.put("index", availSquares.get(i));
@@ -81,14 +79,13 @@ public class Minimax {
 			Map<String, Integer> bestScore = new HashMap<>();
 			bestScore.put("score", 1000);
 			for (int i = 0; i < availSquares.size(); i++) {
-				//boardMinMAx[availSquares.get(i)] = player;
 				logic.setPosition(availSquares.get(i));
 				logic.setPlayers(nextPlayer, currentPlayer);
 				logic.setBoard(boardMinMAx);
 				logic.checkNextItem();
 				char[] newBoard = logic.getNewBoard();
 				Map<String, Integer> result = minimaxSearch(newBoard, currentPlayer, depth+1);
-				// Find the MINIMUM score
+				// Find the MINIMUM score.
 				if (result.get("score").doubleValue() < bestScore.get("score").doubleValue()) {
 					bestScore.put("score", result.get("score"));
 					bestScore.put("index", availSquares.get(i));
@@ -105,24 +102,18 @@ public class Minimax {
 			if (boardMinMAx[i] == player) {
 				if (player == currentPlayer) {
 					score += weightedBoard[i];
+					score += availableSquaresNum;
 				} else {
 					score -= weightedBoard[i];
+					score -= availableSquaresNum;
 				}
 			}
-		}
-		// Test for mobility.
-		if (player == currentPlayer) {
-			score += availableSquaresNum;
-		} else {
-			// Should these be + or - ???
-			score -= availableSquaresNum;
 		}
 		return score;
 	}
 
 	private ArrayList<Integer> evaluateBoard(char[] boardEval, char player) {
 		ArrayList<Integer> availSquares = new ArrayList<Integer>();
-		//char[] boardEvalClone = boardEval.clone();
 		if (player == 'w') {
 			logic.setPlayers('w', 'b');
 		} else {
@@ -137,14 +128,12 @@ public class Minimax {
 				logic.setPosition(position);
 				boolean successfulMove = logic.checkNextItem();
 				if (successfulMove) {
-					//System.out.println(i);
 					// This move is available -> add it to the array.
 					availSquares.add(i);
 				}
 			}
 		}
 		// Return an array of available squares (empty if non available).
-		//System.out.println(availSquares);
 		return availSquares;
 	}
 }
